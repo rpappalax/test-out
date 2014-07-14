7d36fc61fa3ef26eca90ae715bde665e6653518f
Richard Pappalardo <rpappalax@gmail.com>
2014-07-13 23:56:08 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name = "test-out",
     version = "1.0.0",
-    description = "Class to provide helpful logging, test run data and summary statistics for automated tests",
+    description = "Python class to provide helpful logging, test run data and summary statistics for automated tests.",
     author = "Richard Pappalardo",
     author_email = "rpappalax@gmail.com",
     url = "https://github.com/rpappalax/test-out",
